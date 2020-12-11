4b36cf25bd60186ff9dedf6f13e490c74ed9fefa
Larry Baltz <larry@baltz.org>
2020-12-11 11:43:08 +0100

    mix boilerplate

---
+++ lib/elevator.ex
@@ -0,0 +1,18 @@
+defmodule Elevator do
+  @moduledoc """
+  Documentation for `Elevator`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Elevator.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
